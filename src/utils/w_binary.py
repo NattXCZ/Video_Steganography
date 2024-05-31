b79044df6dd58efeed4d754b8dd9622fa9fd23e2
NattXCZ <natalka.joly@seznam.cz>
2024-05-31 19:33:55 +0200

    .

--- src/utils/w_binary.py
+++ src/utils/w_binary.py
@@ -1,4 +1,3 @@
-import magic
 import numpy as np
 
 
@@ -10,6 +9,7 @@ def file_to_binary_1D_arr(file_path):
         print(f"[INFO] The file '{file_path}' has been successfully converted to a 1D numpy array.")
     return binary_array
 
+
 def binary_1D_arr_to_file(binary_array, file_path):
     """Converts a 1D numpy array of binary values back to a file."""
     binary_string = ''.join(binary_array.astype(str))
@@ -19,25 +19,39 @@ def binary_1D_arr_to_file(binary_array, file_path):
     with open(file_path, 'wb') as file:
         file.write(byte_arr)
         print(f"[INFO] The binary array has been successfully converted to the file '{file_path}'.")
-        
-     
+      
+      
+
+
+def string_to_binary_array(orig_string):
+    binary_list = ['{0:08b}'.format(ord(char)) for char in orig_string]
+    binary_string = ''.join(binary_list)
+    binary_array = np.array([int(bit) for bit in binary_string], dtype=np.uint8)
+    
+    return binary_array
+
+
+def binary_array_to_string(binary_array):
+    binary_string = ''.join(binary_array.astype(str))
+    byte_list = [binary_string[i:i+8] for i in range(0, len(binary_string), 8)]
+    chars = [chr(int(byte, 2)) for byte in byte_list]
+    orig_string = ''.join(chars)
+    
+    return orig_string
+
+
+###########
 def add_EOT_sequence(message):
     sequence = np.array([1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0])
     new_message = np.concatenate((message, sequence))
     return new_message
+
 
 def check_EOT_sequence(message):
     sequence = np.array([1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0])
     return np.array_equal(message[-48:], sequence)
 
 
-#------------------------------------------------------testing
-def iterate_over_4_bits(binary_data):
-    """Iterates over the binary data in 4-bit chunks and prints them."""
-
-    for i in range(0, len(binary_data), 4):
-        four_bits = binary_data[i:i+4]  # Extract a 4-bit chunk
-        print(four_bits, end=" ")  # Print the 4-bit chunk
 
 
 
