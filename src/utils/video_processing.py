0ed4c390f97151126c6549a89846c54ac3c558c4
NattXCZ <natalka.joly@seznam.cz>
2024-05-31 09:57:58 +0200

    changed has_audio

--- src/utils/video_processing.py
+++ src/utils/video_processing.py
@@ -1,7 +1,16 @@
 import os
 import shutil
 import cv2
+import numpy as np
 from subprocess import run ,call ,STDOUT ,PIPE
+
+temporal_folder = "./tmp"
+y_folder = "./tmp/Y"
+u_folder = "./tmp/U"
+v_folder = "./tmp/V"
+rgb_folder = "./frames"
+
+
 
 def decode_fourcc(cc):
     """
@@ -25,15 +34,18 @@ def delete_tmp(path="./tmp"):
         print("[INFO] tmp files are cleaned up")
 
 
-def has_audio_track(filename, ffprobe_path = r".\src\utils\ffprobe.exe"):
-
+def has_audio_track(filename, ffprobe_path=r".\src\utils\ffprobe.exe"):
     """Check if the given video file contains audio streams."""
-    result = run([ffprobe_path, "-v", "error", "-show_entries",
-                            "format=nb_streams", "-of",
-                            "default=noprint_wrappers=1:nokey=1", filename],
-        stdout=PIPE,
-        stderr=STDOUT)
-    return (int(result.stdout) -1)
+    result = run([ffprobe_path, "-loglevel", "error", "-show_entries",
+                             "stream=codec_type", "-of", "csv=p=0", filename],
+                            stdout=PIPE,
+                            stderr=PIPE,
+                            text=True)
+    
+    # Check if 'audio' is in the result
+    if 'audio' in result.stdout.split():
+        return True
+    return False
 
 
 def get_vid_stream_bitrate(filename, ffprobe_path = r".\src\utils\ffprobe.exe"):
@@ -60,11 +72,11 @@ def video_to_rgb_frames(video_path):
         video_path (str): The path to the input video file.
         
     """
-
-    if not os.path.exists("./tmp"):
-        os.makedirs("tmp")
-    temporal_folder="./tmp"
-    print("[INFO] tmp directory is created")
+    #!tady bylo driv tmp
+    if not os.path.exists("./frames"):
+        os.makedirs("frames")
+    temporal_folder="./frames"
+    print("[INFO] frames directory is created")
 
 
     capture = cv2.VideoCapture(video_path)
@@ -144,7 +156,12 @@ def video_to_yuv_frames(video_path):
         frame_name = f"frame_{frame_count}.png"
       
         # Split frame into YUV components
-        YUV_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2YUV)
+        #!zmeneno z BGR na RGB
+        #YUV_frame = cv2.cvtColor(frame, cv2.COLOR_BRG2YUV)
+        #YUV_frame = cv2.cvtColor(frame, cv2.COLOR_RGB2YUV)
+        YUV_frame = cv2.cvtColor(frame, cv2.COLOR_RGB2YCrCb)
+        
+        
         Y, U, V = cv2.split(YUV_frame)
 
         # Save Y, U, V components into respective folders
@@ -172,7 +189,9 @@ def reconstruct_video_from_rgb_frames(file_path, properties, ffmpeg_path = r".\s
     #! file_path je tady jen skrz zvuk a koncovku
     fps = properties["fps"]
     codec =  decode_fourcc(properties["codec"])
-    file_extension =  file_path.rsplit(".", 1)[1]
+    #!file_extension =  file_path.rsplit(".", 1)[1]
+    #FIXME:
+    file_extension = "avi"
     bitrate = get_vid_stream_bitrate(file_path)
 
     if has_audio_track(file_path):
@@ -223,8 +242,11 @@ def merge_yuv_to_rgb(y_folder, u_folder, v_folder, output_folder):
         yuv_frame = cv2.merge([y_frame, u_frame, v_frame])
 
         # from YUV to RGB
-        rgb_frame = cv2.cvtColor(yuv_frame, cv2.COLOR_YUV2BGR)
-
+        #!zmeneno z BGR na RGB
+        #rgb_frame = cv2.cvtColor(yuv_frame, cv2.COLOR_YUV2BGR)
+        #rgb_frame = cv2.cvtColor(yuv_frame, cv2.COLOR_YUV2RGB)
+        rgb_frame = cv2.cvtColor(yuv_frame, cv2.COLOR_YCrCb2RGB)
+        
         frame_name = os.path.splitext(y_file)[0] + ".png"
         cv2.imwrite(os.path.join(output_folder, frame_name), rgb_frame)
         
@@ -241,3 +263,86 @@ def reconstruct_video_from_yuv_frames(file_path, properties, ffmpeg_path = r".\s
     reconstruct_video_from_rgb_frames(file_path, properties, ffmpeg_path)
     
     print("[INFO] video is reconstructed")
+    
+    
+    
+    
+def rgb2yuv(image_name):
+    # Load the RGB image
+    frame_path = os.path.join(rgb_folder, image_name)
+    rgb_image = cv2.imread(frame_path)
+    
+    if rgb_image is None:
+        print(f"Error: Unable to load image {frame_path}")
+        return
+    
+    yuv_image = cv2.cvtColor(rgb_image, cv2.COLOR_RGB2YCrCb)
+    #yuv_image = cv2.cvtColor(rgb_image, cv2.COLOR_RGB2YUV)
+
+    # Split YUV channels
+    Y = yuv_image[:, :, 0]
+    U = yuv_image[:, :, 1]
+    V = yuv_image[:, :, 2]
+
+    # Save each channel as a separate image
+    y_path = os.path.join(y_folder, image_name)
+    u_path = os.path.join(u_folder, image_name)
+    v_path = os.path.join(v_folder, image_name)
+
+    cv2.imwrite(y_path, Y)
+    cv2.imwrite(u_path, U)
+    cv2.imwrite(v_path, V)
+
+def yuv2rgb(image_name):
+    # Load the 3 separate images
+    y_path = os.path.join(y_folder, image_name)
+    u_path = os.path.join(u_folder, image_name)
+    v_path = os.path.join(v_folder, image_name)
+
+    Y = cv2.imread(y_path, cv2.IMREAD_GRAYSCALE)
+    U = cv2.imread(u_path, cv2.IMREAD_GRAYSCALE)
+    V = cv2.imread(v_path, cv2.IMREAD_GRAYSCALE)
+    
+    if Y is None or U is None or V is None:
+        print(f"Error: Unable to load one or more YUV images for {image_name}")
+        return
+
+    yuv_image = np.stack((Y, U, V), axis=-1)
+
+    # Convert YUV image to RGB
+    rgb_image = cv2.cvtColor(yuv_image, cv2.COLOR_YCrCb2RGB)
+    #rgb_image = cv2.cvtColor(yuv_image, cv2.COLOR_YUV2RGB)
+
+    # Save the reversed RGB image in the appropriate folder
+    rgb_path = os.path.join(rgb_folder, image_name)
+    cv2.imwrite(rgb_path, rgb_image)
+    
+    
+    
+    
+def create_dirs():
+    if not os.path.exists(temporal_folder):
+        os.makedirs("tmp")
+        
+    y_path = os.path.join(temporal_folder, "Y")
+    u_path = os.path.join(temporal_folder, "U")
+    v_path = os.path.join(temporal_folder, "V")
+    
+    os.makedirs(y_path, exist_ok=True)
+    os.makedirs(u_path, exist_ok=True)
+    os.makedirs(v_path, exist_ok=True)
+    
+    
+def remove_dirs():
+    if os.path.exists(temporal_folder):
+        shutil.rmtree(temporal_folder)
+        print(f"[INFO] Removed {temporal_folder} and its subdirectories.")
+    else:
+        print(f"[INFO] {temporal_folder} does not exist.")
+        
+        
+    if os.path.exists(rgb_folder):
+        shutil.rmtree(rgb_folder)
+        print(f"[INFO] Removed {rgb_folder}.")
+    else:
+        print(f"[INFO] {rgb_folder} does not exist.")
