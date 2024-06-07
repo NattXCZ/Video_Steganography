cb5b8bd5fb66b3521410f234f80b75484bcddc57
NattXCZ <natalka.joly@seznam.cz>
2024-06-07 17:43:33 +0200

    .

--- tests/testing.py
+++ tests/testing.py
@@ -3,51 +3,66 @@ import os
 import numpy as np
 import cv2
 
-from src.methods import hamming_code
+from src.methods import hamming_code as hmc
 from src.utils import w_binary as bnr
 
 
 if __name__ == "__main__":
-    message_file = r"angel_secret.txt"
-    message_text = "Lorem ipsum dolor sit amet congue. Ut consectetuer lorem est labore dolor. Ut quis assum erat at dolores zzril justo aliquyam aliquyam nibh eirmod labore est voluptua te sit illum. Feugiat eirmod vel no invidunt facilisis. Sed amet et vero consetetur eos sea tempor luptatum voluptua. Dolore labore sanctus et justo erat sed rebum accusam lorem illum elitr accusam takimata nonumy. Sanctus erat consequat nobis nisl amet eleifend et rebum justo lorem clita nisl. Sit velit eleifend sadipscing rebum ex. Veniam et lorem aliquyam sanctus exerci consetetur. No clita amet eirmod vulputate duo vero amet sadipscing. Magna nobis stet nonumy et aliquam. Gubergren accusam ipsum facilisi sed."
-    video_path = r"cute_kitten_playing_w_tail.mov"
-   
+
+    #typy toho co muzeme vkladat
+    message_text_file = r"angel_secret.txt"
+    message_text_string = "Lorem ipsum dolor sit amet congue. Ut consectetuer lorem est labore dolor. Ut quis assum erat at dolores zzril justo aliquyam aliquyam nibh eirmod labore est voluptua te sit illum. Feugiat eirmod vel no invidunt facilisis. Sed amet et vero consetetur eos sea tempor luptatum voluptua. Dolore labore sanctus et justo erat sed rebum accusam lorem illum elitr accusam takimata nonumy. Sanctus erat consequat nobis nisl amet eleifend et rebum justo lorem clita nisl. Sit velit eleifend sadipscing rebum ex. Veniam et lorem aliquyam sanctus exerci consetetur. No clita amet eirmod vulputate duo vero amet sadipscing. Magna nobis stet nonumy et aliquam. Gubergren accusam ipsum facilisi sed."
+    message_png_file = r"./carousel-horse.png"
+    
+    #cesty k video souborum (vstupni video ve ktere vkladame a stego video)        
+    video_path = r"input_video.mp4"
+    stego_video_path = r"video.avi"
+    
+    #klice
     key_1 = 10
     key_2 = 20
     key_3 = 2
 
+    #kam zapsat tajnou zpravu
     output_path = r"./decoded_message.txt"
+    output_path_png = r"./carousel-horse_decoded.png"
     
     
-    #ridi jestli vkladame nebo extrahujeme
-    flag = 2
+    #promenna flag ridi jakou cast testujeme
+    flag = 4
+    
+    #promenna shuffle ridi jestli chceme vyuzivat michani pixelu snimku pred vlozenim - s nim vypocet někdy trva dlouho proto je ted na False
+    #ale lze prepnout na True
+    shuffle = False
+    
+    #jestli je True - extrahuje snimky z videa, vytvori z RGB snimku ty YUV snimky
+    #jestli False - bude provadet vsechny operace na jiz extrahovanych YUV snimcich ktere zustaly 
+    # po metode "hamming_encode" (v te se ale musí flag_delete_dirs nastavit na False - to ale resi tato promenna taky)
+    recontructed_vid = False
     
     
+    #SOUBOR
     if flag == 1:
-        #zakodovani souboru do videa
-        hamming_code.hamming_encode(video_path,message_file,key_1,key_2,key_3)
+        #zakodovani souboru do videa (tady zakoduje obrazek png)
+        hmc.hamming_encode(video_path,message_png_file, key_1, key_2, key_3, shuffle_flag = shuffle, flag_delete_dirs = recontructed_vid)
         
     elif flag == 2:
+        #dekodovani souboru z videa (zde png)
+        
+        message_len_file = len(bnr.file_to_binary_1D_arr(message_png_file))
+        hmc.hamming_decode(stego_video_path, key_1, key_2, key_3, message_len_file, output_path_png, shuffle_flag = shuffle, flag_recostr_vid = recontructed_vid)
 
-        #zakodovani retezce do videa
-        hamming_code.hamming_encode(video_path,message_file,key_1,key_2,key_3,True)
 
+
+    #STRING
     elif flag == 3:
-        #dekodovani souboru z videa
-        stego_video_path = r"video.avi"
-        message_len_file = len(bnr.file_to_binary_1D_arr(message_file))
-        hamming_code.hamming_decode(stego_video_path, key_1,key_2,key_3,message_len_file, output_path)
-
+        #zakodovani retezce do videa
+        hmc.hamming_encode(video_path,message_text_string, key_1, key_2, key_3, string_flag = True, shuffle_flag = shuffle,flag_delete_dirs = recontructed_vid)
+        
+        
     elif flag == 4:
         #dekodovani retezce z videa
-        stego_video_path = r"video.avi"
-        message_len_string = len(bnr.string_to_binary_array(message_text))
-        
-        hamming_code.hamming_decode(stego_video_path, key_1,key_2,key_3,message_len_string, output_path, True)
 
-
-
-    
-    
-    
+        message_len_string = len(bnr.string_to_binary_array(message_text_string))
+        hmc.hamming_decode(stego_video_path, key_1, key_2, key_3, message_len_string, output_path, string_flag = True, shuffle_flag = shuffle, flag_recostr_vid = recontructed_vid)
 
